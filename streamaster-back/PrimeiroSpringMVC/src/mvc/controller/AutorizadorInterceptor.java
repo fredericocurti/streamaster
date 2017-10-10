2c4a544d3771a278f6e2997dad3af2cd69964299
BrunaKimura <brunamk@al.insper.edu.br>
2017-10-10 09:44:03 -0300

    alteraçaõ no back

---
+++ streamaster-back/PrimeiroSpringMVC/src/mvc/controller/AutorizadorInterceptor.java
@@ -0,0 +1,27 @@
+package mvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class AutorizadorInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+		public boolean preHandle(HttpServletRequest request,
+				HttpServletResponse response, Object controller) throws Exception {
+		String uri = request.getRequestURI();
+		if( uri.endsWith("registro") ||
+			uri.endsWith("efetuaRegistro") ||
+			uri.endsWith("loginForm") ||
+			uri.endsWith("efetuaLogin") ||
+			uri.endsWith("getImage") ) {
+			return true;
+		}
+		if(request.getSession().getAttribute("usuarioLogado") != null) {
+			 return true;
+		}
+		response.sendRedirect("loginForm");
+		return false;
+	}
+}
--- streamaster-back/PrimeiroSpringMVC/src/mvc/controller/TarefasController.java
+++ streamaster-back/PrimeiroSpringMVC/src/mvc/controller/TarefasController.java
@@ -1,7 +1,19 @@
 package mvc.controller;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import mvc.model.DAO;
+import mvc.model.Tarefa;
 
 @Controller
 public class TarefasController {
@@ -10,6 +22,34 @@ public class TarefasController {
 	public String execute() {
 		System.out.println("Lógica do MVC");
 		return "info";
+	}
+	
+	@RequestMapping("loginForm")
+	public String loginForm() {
+		return "formulario-login";
+	}
+	
+	@RequestMapping("efetuaLogin")
+	public String efetuaLogin(Tarefa login, HttpSession session) {
+		if(new DAO().existeUsuario(login)) {
+			session.setAttribute("usuarioLogado", login.getName());
+			return "menu";
+		}
+		return "redirect:loginForm";
+	}
+	@RequestMapping("logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:loginForm";
+	}
+	@RequestMapping(value = "getImage", method = RequestMethod.GET)
+	public void showImage(@RequestParam("login") String login, HttpServletResponse
+			response,HttpServletRequest request)
+					throws ServletException, IOException{
+		DAO dao = new DAO();
+		response.setContentType("image/jpeg, image/jpg, image/png, image/gif");
+		response.getOutputStream().write(dao.buscaFoto(login));
+		response.getOutputStream().close();
 	}
 
 }
